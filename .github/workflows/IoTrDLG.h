7447f7f40a0ebe078c2b451c911ef64fed0a769d
Styl-Drama <160217489+Styl-Drama@users.noreply.github.com>
2024-02-16 10:54:19 +0200

    Create IoTrDLG.h

---
+++ .github/workflows/IoTrDLG.h
@@ -0,0 +1,19 @@
+IoTrDLG.h 
+(The dialog box & items Header file - H file in ASCII code)
+
+
+#define DIALOGID_FirstDialogBox       				1
+
+#define TEXTID_FileName       					1
+
+#define PUSHBUTTONID_Tree-display-file-structure        		1
+#define PUSHBUTTONID_VirtualCamera      				2
+#define PUSHBUTTONID_Parametric-&-Relative-modeling  		3
+#define PUSHBUTTONID_Dialogs          				4
+#define PUSHBUTTONID_CAD-event-driven-routines           		5
+#define PUSHBUTTONID_Non-fungible tree-image-tokens		6
+#define PUSHBUTTONID_Tree-CAD-geometry-deployment  	7
+#define PUSHBUTTONID_Tree-shape-modeling-completion        	8
+#define PUSHBUTTONID_START    					9
+#define PUSHBUTTONID_STOP    					10
+
