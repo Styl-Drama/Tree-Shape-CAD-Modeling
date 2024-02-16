d4da56656d0d89208eb090082f4c5d969e030b09
Styl-Drama <160217489+Styl-Drama@users.noreply.github.com>
2024-02-16 10:56:23 +0200

    Create IoTrIDs.h
    Internet of Trees IDs (a header readable file)

---
+++ .github/workflows/IoTrIDs.h
@@ -0,0 +1,13 @@
+IoTrIDs.h 
+(The resources IDs - Include file in ASCII code)
+	
+/* dialog box ID */
+#define TREEdialogBox		1
+		
+/* Dialog Hook Functions IDs */
+#define HkCamera			1
+……………………..
+
+		/* Push Button Items IDs */
+#define VirtualCamera		2
+
